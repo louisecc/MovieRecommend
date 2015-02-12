ece4df91e39818b0e1b405eef3815a58b87e3d95
louisecc <susan9009@hotmail.com>
2015-02-12 13:49:17 -0800

    Create HdfsDAO.java

---
+++ HdfsDAO.java
@@ -0,0 +1,106 @@
+package myhadoop;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.mapred.JobConf;
+
+public class HdfsDAO {
+
+    private static final String HDFS = "hdfs://localhost:9000/";
+
+    public HdfsDAO(Configuration conf) {
+        this(HDFS, conf);
+    }
+
+    public HdfsDAO(String hdfs, Configuration conf) {
+        this.hdfsPath = hdfs;
+        this.conf = conf;
+    }
+
+    private String hdfsPath;
+    private Configuration conf;
+
+    public static JobConf config() {
+        JobConf conf = new JobConf(HdfsDAO.class);
+        conf.setJobName("HdfsDAO");
+        return conf;
+    }
+
+    public void mkdirs(String folder) throws IOException {
+        Path path = new Path(folder);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        if (!fs.exists(path)) {
+            fs.mkdirs(path);
+        }
+        fs.close();
+    }
+
+    public void rmr(String folder) throws IOException {
+        Path path = new Path(folder);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        fs.deleteOnExit(path);
+        fs.close();
+    }
+
+    public void rename(String src, String dst) throws IOException {
+        Path name1 = new Path(src);
+        Path name2 = new Path(dst);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        fs.rename(name1, name2);
+        fs.close();
+    }
+
+    public void ls(String folder) throws IOException {
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        fs.close();
+    }
+
+    public void createFile(String file, String content) throws IOException {
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        byte[] buff = content.getBytes();
+        FSDataOutputStream os = null;
+        try {
+            os = fs.create(new Path(file));
+            os.write(buff, 0, buff.length);
+            System.out.println("Create: " + file);
+        } finally {
+            if (os != null)
+                os.close();
+        }
+        fs.close();
+    }
+
+    public void copyFile(String local, String remote) throws IOException {
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        fs.copyFromLocalFile(new Path(local), new Path(remote));
+        fs.close();
+    }
+
+    public void download(String remote, String local) throws IOException {
+        Path path = new Path(remote);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        fs.copyToLocalFile(path, new Path(local));
+        fs.close();
+    }
+
+    public void cat(String remoteFile) throws IOException {
+        Path path = new Path(remoteFile);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        FSDataInputStream fsdis = null;
+        try {
+            fsdis = fs.open(path);
+            IOUtils.copyBytes(fsdis, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(fsdis);
+            fs.close();
+        }
+    }
+
+}
