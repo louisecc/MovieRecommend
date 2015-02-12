0c58b4052c05dbbd0ff45ef50d8f6617f8145df7
louisecc <susan9009@hotmail.com>
2015-02-12 14:19:57 -0800

    Create MatrixMultiply.java

---
+++ MatrixMultiply.java
@@ -0,0 +1,108 @@
+package recommend;
+
+import java.io.IOException;
+import java.util.Map;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.RunningJob;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+import myhadoop.HdfsDAO;
+
+public class MatrixMultiply {
+
+    public static class MMMapper extends MapReduceBase implements Mapper<LongWritable, Text, IntWritable, Text> {
+        private final static IntWritable k = new IntWritable();
+        private final static Text v = new Text();
+
+        @Override
+        public void map(LongWritable key, Text values, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
+            String[] tokens = Recommend.DELIMITER.split(values.toString());
+            for (int i = 1; i < tokens.length; i++) {
+                String[] vector = tokens[i].split(":");
+                int movieID = Integer.parseInt(vector[0]);
+                String pref = vector[1];
+
+                k.set(movieID);
+                v.set(tokens[0] + ":" + pref);
+                output.collect(k, v);
+            }
+        }
+    }
+
+    public static void run1(Map<String, String> path) throws IOException {
+        JobConf conf = Recommend.config();
+
+        String input = path.get("3rdInput1");
+        String output = path.get("3rdOutput1");
+
+        HdfsDAO hdfs = new HdfsDAO(Recommend.HDFS, conf);
+        hdfs.rmr(output);
+
+        conf.setOutputKeyClass(IntWritable.class);
+        conf.setOutputValueClass(Text.class);
+
+        conf.setMapperClass(MMMapper.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(input));
+        FileOutputFormat.setOutputPath(conf, new Path(output));
+
+        RunningJob job = JobClient.runJob(conf);
+        while (!job.isComplete()) {
+            job.waitForCompletion();
+        }
+    }
+
+    public static class MatrixWrapperMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static Text k = new Text();
+        private final static IntWritable v = new IntWritable();
+
+        @Override
+        public void map(LongWritable key, Text values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String[] tokens = Recommend.DELIMITER.split(values.toString());
+            k.set(tokens[0]);
+            v.set(Integer.parseInt(tokens[1]));
+            output.collect(k, v);
+        }
+    }
+
+    public static void run2(Map<String, String> path) throws IOException {
+        JobConf conf = Recommend.config();
+
+        String input = path.get("3rdInput2");
+        String output = path.get("3rdOutput2");
+
+        HdfsDAO hdfs = new HdfsDAO(Recommend.HDFS, conf);
+        hdfs.rmr(output);
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(MatrixWrapperMapper.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(input));
+        FileOutputFormat.setOutputPath(conf, new Path(output));
+
+        RunningJob job = JobClient.runJob(conf);
+        while (!job.isComplete()) {
+            job.waitForCompletion();
+        }
+    }
+
+}
